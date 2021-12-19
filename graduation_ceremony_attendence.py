264e46d663006c2417a91d82ee7d43b0f264c2a5
mohanj1919 <mohankumar1919@gmail.com>
2021-12-19 22:58:04 +0530

    accept users input
    add input validations

--- graduation_ceremony_attendence.py
+++ graduation_ceremony_attendence.py
@@ -1,26 +1,5 @@
 import itertools
 
-"""
-## Question
-
-In a university, your attendance determines whether you will be allowed to attend your graduation ceremony. 
-You are not allowed to miss classes for four or more consecutive days. 
-Your graduation ceremony is on the last day of the academic year, which is the Nth day.
-
-Your task is to determine the following:
-
-1. The number of ways to attend classes over N days.
-2. The probability that you will miss your graduation ceremony.
-Represent the solution in the string format as "Answer of (2) / Answer of (1)", don't actually divide or reduce the fraction to decimal
-
-Test cases:
-
-for 5 days: 14/29
-
-for 10 days: 372/773
-
-"""
-
 def get_attendance_possibilities(n):
     for i in itertools.product("01", repeat=n):
         yield ",".join(i)
@@ -37,5 +16,12 @@ def find_attendence_possibilities(n):
                 graduation_missed_scenarios += 1
     print(f"{graduation_missed_scenarios}/{valid_attendence}")
 
-find_attendence_possibilities(5)
-find_attendence_possibilities(10)
+value = input("Enter a value:", )
+try:
+    value = int(value)
+    if value <= 0:
+        print(f"Enter a positive value as input. '{value}' is invalid.")
+        exit()
+    find_attendence_possibilities(int(value))
+except ValueError as e:
+    print(f"{value} is invalid. Enter a positive integer value")
